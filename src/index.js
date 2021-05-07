05c56736d53d596fbfee4c5fb02fa32dea06ab79
shotius <sh.archemashvili@gmail.com>
2021-05-07 21:19:50 +0400

    done animated slider on exercise using vanila js

---
+++ src/index.js
@@ -0,0 +1,23 @@
+function gym() {
+    document.getElementById('exercise-slider-pic')
+    .src='/assests/Mail_Image.png'
+
+    document.getElementById('exercise-slider-text')
+    .innerHTML = '<div class="ex-slider-header">A short title works best </div>At home to gym, crossfit to pilates, yoga to bodyweight, as well as healthy and easy to make breakfasts, lunches, dinners and snacks. We want to make sure you enjoy the journey for a healthy lifestyle.</div>'
+}
+
+function home() {
+    document.getElementById('exercise-slider-pic')
+    .src="/assests/ladyathome.jpg"
+
+    document.getElementById('exercise-slider-text')
+    .innerHTML = "<div class='ex-slider-header'>Working out at home</div> the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "
+}
+
+function yoga() {
+    document.getElementById('exercise-slider-pic')
+    .src='/assests/yogalady.jpg'
+
+    document.getElementById('exercise-slider-text')
+    .innerHTML = '<div class="ex-slider-header">Doing yoga </div> not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with des.</div>'
+}
